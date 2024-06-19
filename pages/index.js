6d8647483cde318950ca8adcab89620de1979e17
Mcclares <86326552+Mcclares@users.noreply.github.com>
2024-06-19 21:46:25 +0300

    New Commit

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Promo24</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-
+        <div className='promo'>
+            <p>Promo:</p>
+        </div>
       </main>
 
-      <Footer />
+
     </div>
   )
 }
